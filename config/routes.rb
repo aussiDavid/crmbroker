49018175e3dcb2014cea06118357e12a37ee0bdb
David Milanese <milanese.david@gmail.com>
2017-04-27 09:23:38 +0930

    remove unused resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-
   
   devise_for :accounts, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
 
-  # resource :account, only: %i{update edit}
   resource :account, only: %i{update edit show}
+
   authenticated :account do
     root 'accounts#show', as: :authenticated_root
   end
