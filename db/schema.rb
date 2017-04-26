6e291aba7c256933b839f953ee439a2991f1e779
David Milanese <milanese.david@gmail.com>
2017-04-27 09:22:56 +0930

    Updated the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215134731) do
+ActiveRecord::Schema.define(version: 20170315011747) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -33,6 +33,16 @@ ActiveRecord::Schema.define(version: 20170215134731) do
     t.index ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "connections", force: :cascade do |t|
+    t.integer  "account_id"
+    t.integer  "crm_id"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_connections_on_account_id"
+    t.index ["crm_id"], name: "index_connections_on_crm_id"
   end
 
   create_table "crms", force: :cascade do |t|
